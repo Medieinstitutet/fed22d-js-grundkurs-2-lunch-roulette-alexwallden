df3d68ddb445eaa258513a2cf6bc52564eb76ad1
alexwallden <alexander.wallden@me.com>
2022-12-07 16:52:44 +0100

    :art: updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,37 +5,18 @@ import './style/style.scss';
 // import userCoordinates from './inventory/userCoordinates';
 import Coordinates from './models/Coordinates';
 
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const google: any;
 const app: HTMLDivElement | null = document.querySelector('#app');
 
-const getUserCoordinates = () => new Promise((resolve, reject) => {
-  navigator.geolocation.getCurrentPosition(
-    (position) => resolve(new Coordinates(position.coords.latitude, position.coords.longitude)),
-    (error) => {
-      if (error.code === 1 && app !== null) {
-        app.innerHTML = 'Du behöver aktivera platstjänster';
-      }
-      reject(console.error(error.message));
-    },
-  );
-});
+let userCoordinates: Coordinates | null = null;
 
-const userCoordinates: Coordinates = await getUserCoordinates() as Coordinates;
-
-// function userCoordinates() {
-//   getUserCoordinates()
-//     .then((coords) => coords as Coordinates)
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// }
-
-if (app !== null) {
-  app.innerHTML = `Din position är ${userCoordinates.lat} och ${userCoordinates.lng}`;
-}
-
-// Initialize and add the map
 function initMap(): void {
   const map = new google.maps.Map(
     document.getElementById('map') as HTMLElement,
@@ -44,20 +25,45 @@ function initMap(): void {
       center: userCoordinates,
     },
   );
-
   const marker = new google.maps.Marker({
     position: userCoordinates,
     map,
   });
-
   console.log(marker);
 }
 
-declare global {
-  interface Window {
-    initMap: () => void;
-  }
-}
-window.initMap = initMap;
+const run = async () => {
+  const getUserCoordinates = () => new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => resolve(new Coordinates(position.coords.latitude, position.coords.longitude)),
+      (error) => {
+        if (error.code === 1 && app !== null) {
+          app.innerHTML = 'Du behöver aktivera platstjänster';
+        }
+        reject(console.error(error.message));
+      },
+    );
+  });
+  userCoordinates = await getUserCoordinates() as Coordinates;
 
-initMap();
+  if (app !== null) {
+    app.innerHTML = `Din position är ${userCoordinates.lat} och ${userCoordinates.lng}`;
+  }
+  window.initMap = initMap;
+  initMap();
+};
+// function userCoordinates() {
+//   getUserCoordinates()
+//     .then((coords) => coords as Coordinates)
+//     .catch((err) => {
+//       console.error(err);
+//     });
+// }
+
+run()
+  .then((x) => {
+    console.log(x);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
