af7253bdb7e823c25d010242e6e66bc0e0809d90
alexwallden <alexander.wallden@me.com>
2022-12-07 13:35:48 +0100

    :construction: deploy

--- src/main.ts
+++ src/main.ts
@@ -6,28 +6,28 @@ const app: HTMLDivElement | null = document.querySelector('#app');
 if (app !== null) { app.innerHTML = `Din position är ${userCoordinates.lat} och ${userCoordinates.lng}`; }
 
 // Initialize and add the map
-function initMap(): void {
-  // The map, centered at Uluru
-  const map = new google.maps.Map(
-    document.getElementById("map") as HTMLElement,
-    {
-      zoom: 16,
-      center: userCoordinates,
-    }
-  );
+// function initMap(): void {
+//   // The map, centered at Uluru
+//   const map = new google.maps.Map(
+//     document.getElementById("map") as HTMLElement,
+//     {
+//       zoom: 16,
+//       center: userCoordinates,
+//     }
+//   );
 
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: userCoordinates,
-    map: map,
-  });
-}
+//   // The marker, positioned at Uluru
+//   const marker = new google.maps.Marker({
+//     position: userCoordinates,
+//     map: map,
+//   });
+// }
 
-declare global {
-  interface Window {
-    initMap: () => void;
-  }
-}
-window.initMap = initMap;
+// declare global {
+//   interface Window {
+//     initMap: () => void;
+//   }
+// }
+// window.initMap = initMap;
 
-initMap();
+// initMap();
