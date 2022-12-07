b2f30e9c52c0c859e1df47a209f96ca1c481e847
alexwallden <alexander.wallden@me.com>
2022-12-07 14:59:12 +0100

    declared const google as any and added places API script

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,12 @@
 import './style/style.scss';
 import userCoordinates from './inventory/userCoordinates';
 
+declare const google: any;
 const app: HTMLDivElement | null = document.querySelector('#app');
 
-if (app !== null) { app.innerHTML = `Din position är ${userCoordinates.lat} och ${userCoordinates.lng}`; }
+if (app !== null) {
+  app.innerHTML = `Din position är ${userCoordinates.lat} och ${userCoordinates.lng}`;
+}
 
 // Initialize and add the map
 function initMap(): void {
