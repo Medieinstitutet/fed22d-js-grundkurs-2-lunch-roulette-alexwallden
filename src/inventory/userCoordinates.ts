0c15fcedd30ed56ab1ac1a2766f4183890ac5851
alexwallden <alexander.wallden@me.com>
2022-12-07 18:32:20 +0100

    :sparkles: working restaurant request

--- src/inventory/userCoordinates.ts
+++ src/inventory/userCoordinates.ts
@@ -1,20 +1,16 @@
 import Coordinates from '../models/Coordinates';
 
-const app = document.querySelector('#app');
-
-const getUserCoordinates = () => new Promise((resolve, reject) => {
+const getUserCoordinates = (): Promise<Coordinates | null> => new Promise((resolve, reject) => {
   navigator.geolocation.getCurrentPosition(
     (position) => resolve(new Coordinates(position.coords.latitude, position.coords.longitude)),
     (error) => {
-      if (error.code === 1 && app !== null) {
-        app.innerHTML = 'Du behöver aktivera platstjänster';
+      if (error.code === 1) {
+        resolve(null);
+        return;
       }
       reject(console.error(error.message));
     },
   );
 });
 
-// eslint-disable-next-line import/no-mutable-exports
-const userCoordinates: Coordinates = await getUserCoordinates() as Coordinates;
-
-export default userCoordinates;
+export default getUserCoordinates;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+/* eslint-disable max-len */
+/* eslint-disable no-new */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import './style/style.scss';
-// import userCoordinates from './inventory/userCoordinates';
+import getUserCoordinates from './inventory/userCoordinates';
 import Coordinates from './models/Coordinates';
 
 declare global {
@@ -13,56 +15,67 @@ declare global {
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const google: any;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let map: any;
 const app: HTMLDivElement | null = document.querySelector('#app');
 
 let userCoordinates: Coordinates | null = null;
 
+function retrieveRestaurants(radius: string) {
+  const request = {
+    location: userCoordinates,
+    radius,
+    type: ['restaurant'],
+  };
+
+  // Skriv ut resultaten på kartan
+  function handleResults(results: string | any[], status: any) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      for (let i = 0; i < results.length; i++) {
+        // printa en kartnål
+        console.log(results[i]);
+        const restaurant = results[i];
+        const position = new Coordinates(restaurant.geometry.location.lat() as number, restaurant.geometry.location.lng() as number);
+        new google.maps.Marker({
+          position,
+          map,
+        });
+      }
+    }
+  }
+
+  // Gör en sökning… vänta på resultaten
+  const service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, handleResults);
+}
+
 function initMap(): void {
-  const map = new google.maps.Map(
-    document.getElementById('map') as HTMLElement,
-    {
-      zoom: 16,
-      center: userCoordinates,
-    },
-  );
-  const marker = new google.maps.Marker({
+  map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    zoom: 16,
+    center: userCoordinates,
+  });
+  // eslint-disable-next-line no-new
+  new google.maps.Marker({
     position: userCoordinates,
     map,
   });
-  console.log(marker);
+  retrieveRestaurants('1000');
 }
 
 const run = async () => {
-  const getUserCoordinates = () => new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => resolve(new Coordinates(position.coords.latitude, position.coords.longitude)),
-      (error) => {
-        if (error.code === 1 && app !== null) {
-          app.innerHTML = 'Du behöver aktivera platstjänster';
-        }
-        reject(console.error(error.message));
-      },
-    );
-  });
-  userCoordinates = await getUserCoordinates() as Coordinates;
-
-  if (app !== null) {
+  userCoordinates = await getUserCoordinates();
+  if (userCoordinates && app) {
     app.innerHTML = `Din position är ${userCoordinates.lat} och ${userCoordinates.lng}`;
+    window.initMap = initMap;
+    initMap();
+  } else if (app) {
+    app.innerHTML = 'Du behöver aktivera platstjänster';
   }
-  window.initMap = initMap;
-  initMap();
 };
-// function userCoordinates() {
-//   getUserCoordinates()
-//     .then((coords) => coords as Coordinates)
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// }
 
 run()
-  .then((x) => {
-    console.log(x);
+  .then(() => {
+    console.log('Appen startad');
   })
   .catch((err) => {
     console.error(err);
